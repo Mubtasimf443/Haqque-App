d51a761e745482b14aa6013653a14bcc4a05b731
Mubtasim <mubtasimf443@gmail.com>
2025-09-02 11:00:52 +0600

    before db connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { BooksService } from './books/books.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, BooksModule],
+  controllers: [AppController, AuthController, UserController],
+  providers: [AppService, BooksService],
 })
 export class AppModule {}
